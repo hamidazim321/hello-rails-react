61da5db377921f73d09bf7743b4e6947320e894d
hamid321 <hamidazim321@gmail.com>
2024-02-12 17:29:48 +0500

    Add root controller

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,25 @@
-// Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  let [counter, setCounter] = useState(0)
+  const increment = () => {
+    setCounter((prev) => prev + 1)
+  }
+  return (
+    <>
+      <h1>
+        Count: {counter}
+      </h1>
+      <button onClick={increment}>increment</button>
+    </>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
