85a1c0504e3c86074c3dc9ee384cb04e4d3a6fc9
hamid321 <hamidazim321@gmail.com>
2024-02-12 17:51:26 +0500

    Add messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'root#index'
+
+  namespace :api do 
+    get '/greetings', to: "messages#random"
+  end
   get "up" => "rails/health#show", as: :rails_health_check
 end
